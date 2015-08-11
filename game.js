aeacbb009b9afa1be91f61c28b00d4387ef245ed
Louis Desportes <louis@akkes.fr>
2015-08-11 11:59:53 +0200

    Basic display struct

--- game.js
+++ game.js
@@ -1,15 +1,47 @@
+var ctx;
+var pixSize;
+var pixHeight;
+
 function play() {
+	resize();
+}
+
+function resize() {
+	position();
+	display();
+}
+
+// Calculate line position & size
+function position() {
 	var canvas = document.getElementById("view");
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
+
 	if (canvas.getContext) {
-		var ctx = canvas.getContext("2d");
+		ctx = canvas.getContext("2d");
 	} else {
 		return;
 	}
 
-	//Set Background
+	// Calculate line position
+	pixSize = (ctx.canvas.width - ctx.canvas.width%50)/50;
+	console.log("size: " + pixSize + ", modulo: " + ctx.canvas.width%50);
+	pixHeight = (ctx.canvas.height - pixSize)/2;
+}
+
+// Update Display
+function display() {
+	// Set Background
 	ctx.fillStyle = "black";
-	ctx.fillRect(0,0, canvas.width, canvas.height);
-	return;
+	ctx.fillRect(0,0, ctx.canvas.width, ctx.canvas.height);
+
+	// Display line
+	ctx.fillStyle = "grey";
+	for (var i = 0; i < 50; i++) {
+		ctx.fillRect(i*pixSize, pixHeight, pixSize, pixSize);
+	}
+
+	// Display Enemies
+
+	// Display Heroes
 }
